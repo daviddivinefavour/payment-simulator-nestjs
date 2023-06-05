7feeb378e9dc19cc66d4bab4f692491b5bc6c344
Divinefavour David <blazer0022@gmail.com>
2023-06-05 14:05:31 +0100

    initial setup

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as path from 'path';
+
+const devPath: string = path.join(__dirname, `../env/.env.development`);
+const prodPath: string = path.join(__dirname, `../env/.env.production`);
+
+// Select the env path based on the command used to start the application server.
+const envPath: string =
+  process.env.NODE_ENV === 'production' ? prodPath : devPath;
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: envPath,
+      ignoreEnvFile: process.env.NODE_ENV === 'production',
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
